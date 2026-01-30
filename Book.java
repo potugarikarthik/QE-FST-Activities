bd6390bce2c99c6f53fb3aae84fa75b41113981c
potugarikarthik <potugarik@gmail.com>
2026-01-30 16:31:02 +0530

    Delete Book.java

--- Book.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public abstract class Book {
-    String title;
-    public abstract void setTitle(String s);
-    public String getTitle() {
-        return title;
-    }
-}
